804e1675e19e9f481cc13896128659bd06fd36b7
Michael Simacek <msimacek@redhat.com>
2017-05-22 16:51:47 +0200

    Load book.authors eagerly

--- core/src/main/java/cz/muni/fi/pv243/model/Book.java
+++ core/src/main/java/cz/muni/fi/pv243/model/Book.java
@@ -6,6 +6,7 @@ import java.util.List;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -38,7 +39,7 @@ public class Book implements Serializable {
     @Column
     private String desciption;
 
-    @ManyToMany(mappedBy = "books")
+    @ManyToMany(mappedBy = "books", fetch = FetchType.EAGER)
     private List<Author> authors = new ArrayList<>();
 
     public List<Author> getAuthors() {
