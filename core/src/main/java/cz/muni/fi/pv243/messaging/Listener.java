242d433b35229b6eb0893fa7c40ba956b2166059
Jirka Kruml <jirka.kruml@gmail.com>
2017-06-20 19:46:53 +0200

    Add handling of JMS messages

--- core/src/main/java/cz/muni/fi/pv243/messaging/Listener.java
+++ core/src/main/java/cz/muni/fi/pv243/messaging/Listener.java
@@ -6,6 +6,8 @@ import javax.jms.JMSException;
 import javax.jms.Message;
 import javax.jms.MessageListener;
 
+import com.google.gson.Gson;
+
 @MessageDriven(activationConfig = {
         @ActivationConfigProperty(propertyName = "destination", propertyValue = "java:/jboss/exported/jms/queue/myQueue"),
         @ActivationConfigProperty(propertyName = "acknowledgeMode", propertyValue = "Auto-acknowledge"),
@@ -16,7 +18,12 @@ public class Listener implements MessageListener {
     @Override
     public void onMessage(Message message) {
         try {
-            System.out.println("received: " + message.getBody(String.class));
+            String json = message.getBody(String.class);
+            JMSMessage msg = new Gson().fromJson(json, JMSMessage.class);
+            System.out.println("A new copy of the book " + msg.getBookTitle() + "has just been ordered.");
+
+            // code to order book
+
         } catch (JMSException e) {
             e.printStackTrace();
         }
