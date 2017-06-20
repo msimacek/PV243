dc2fbcd823036f276c72c4556e7f8b4844b5328a
Michael Simacek <msimacek@redhat.com>
2017-06-20 14:08:34 +0200

    Make the jms listener receive messages

--- core/src/main/java/cz/muni/fi/pv243/messaging/Listener.java
+++ core/src/main/java/cz/muni/fi/pv243/messaging/Listener.java
@@ -1,14 +1,24 @@
 package cz.muni.fi.pv243.messaging;
 
-import javax.ejb.Stateless;
+import javax.ejb.ActivationConfigProperty;
+import javax.ejb.MessageDriven;
+import javax.jms.JMSException;
 import javax.jms.Message;
 import javax.jms.MessageListener;
 
-@Stateless
+@MessageDriven(activationConfig = {
+        @ActivationConfigProperty(propertyName = "destination", propertyValue = "java:/jboss/exported/jms/queue/myQueue"),
+        @ActivationConfigProperty(propertyName = "acknowledgeMode", propertyValue = "Auto-acknowledge"),
+        @ActivationConfigProperty(propertyName = "destinationType", propertyValue = "javax.jms.Queue")
+})
 public class Listener implements MessageListener {
 
     @Override
     public void onMessage(Message message) {
-        System.out.println("received: " + message);
+        try {
+            System.out.println("received: " + message.getBody(String.class));
+        } catch (JMSException e) {
+            e.printStackTrace();
+        }
     }
 }
